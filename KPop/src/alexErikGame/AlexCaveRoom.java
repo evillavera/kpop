7811df56c32c95955bec310969fbb5bc90b87a4b
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-11-06 10:35:41 -0500

    scanner st

--- KPop/src/alexErikGame/AlexCaveRoom.java
+++ KPop/src/alexErikGame/AlexCaveRoom.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class AlexCaveRoom extends NPCRoom {
 	
-	public static Scanner in;
+	//public static Scanner in;
 	
 	public static void main(String[] args){
-		in = new Scanner(System.in);
+		//in = new Scanner(System.in);
+		
+
+	     Scanner sc = new Scanner(System.in);
+	     int i = sc.nextInt();
 	}
 	
 	public AlexCaveRoom() {
@@ -15,7 +19,7 @@ public class AlexCaveRoom extends NPCRoom {
 		int output;
 		System.out.println("Please type in a number");
 		response = in.nextLine();
-		ouput = (parseInt(response)*2)
+		output = (parseInt(response)*2);
 		//response = new Scanner(System.in);
 		//response = in.nextLine();
 	}
