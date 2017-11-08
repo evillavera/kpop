aa8adb18bd9a216544acc945cefcb817d34e0bb0
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-08 11:29:16 -0500

    Yeah boy

--- KPop/src/samJenny/SamRoom.java
+++ KPop/src/samJenny/SamRoom.java
@@ -7,11 +7,13 @@ public class SamRoom extends CaveRoom {
 
 	private String validKeys;
 	private boolean active;
+	private int fail;
 	
 	public SamRoom(String description) {
 		super(description);
 		validKeys = "wdsae";
 		active = true;
+		fail = 0;
 	}
 
 	public String validKeys() {
@@ -24,11 +26,23 @@ public class SamRoom extends CaveRoom {
 				String s = CaveExplorer.in.nextLine();
 				while(!s.equalsIgnoreCase("l")) {
 					CaveExplorer.print("That won't work try again.");
+					fail++;
+					if(fail >=5) {
+						CaveExplorer.currentRoom.leave();
+						CaveExplorer.currentRoom = CaveExplorer.caves[6][3];
+						CaveExplorer.currentRoom.enter();
+						CaveExplorer.inventory.updateMap();
+						break;
+					}
 					s = CaveExplorer.in.nextLine();
 				}
-				CaveExplorer.print("You manage to find a hole in the crowd.\n*You have been freed*");
-				this.active = false;
-				
+				if(CaveExplorer.currentRoom == CaveExplorer.caves[6][3]){
+					CaveExplorer.print("The wave of fans pushed you back!");
+				}
+				else {
+					CaveExplorer.print("You manage to find a hole in the crowd.\n*You have been freed*");
+					this.active = false;
+				}
 		}
 		else {
 			CaveExplorer.print("That key does nothing.");
