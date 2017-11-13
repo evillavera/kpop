ac73ff9251c7e05d79521a2a6549b41ef13725a1
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-11-13 11:43:14 -0500

    pushed battleship starter code

--- KPop/src/alexErikGame/AlexBackEnd.java
+++ KPop/src/alexErikGame/AlexBackEnd.java
@@ -1,7 +1,22 @@
 package alexErikGame;
 
-public class AlexBackEnd {
-	public AlexBackEnd() {
+public class AlexBackEnd implements ErikSupport {
+	
+	private AlexSupport frontend;
+	private AlexErikFleet[][] ships;
+	
+	public AlexBackEnd(AlexSupport frontend) {
+		this.frontend = frontend;
+		ships = new AlexErikFleet[7][7];
+	}
+	
+	public void shipShape() {
+		// TODO Auto-generated method stub
 		
 	}
+	
+	public int getShipsSunk() {
+		return shipsSunk;
+	}
+	
 }
