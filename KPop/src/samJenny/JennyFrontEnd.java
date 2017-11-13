21dd87a656d79ce0814f93a2b75ce67b3c776d35
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-13 11:46:38 -0500

    adsdad

--- KPop/src/samJenny/JennyFrontEnd.java
+++ KPop/src/samJenny/JennyFrontEnd.java
@@ -3,24 +3,29 @@ package samJenny;
 public class JennyFrontEnd implements SamSupport {
 	
 	private static JennySupport backend;
+	private static JennySamPlot[][] plots;
 
 	public JennyFrontEnd() {
 		backend = new SamBackEnd(this);
+		plots = backend.getPlots();
 	}
 
 	public static void main(String[] args) {
 		JennyFrontEnd demo = new JennyFrontEnd();
-		JennySamPlot[][] plots = backend.getPlots();
 		displayField(plots);
 	}
 
 	public static void displayField(JennySamPlot[][] plot) {
 		String rows = "0123456789";
 		String columns = "  0123456789";
-	}
-
-	public void play() {
-		
+		for(int row = 0; row < plots.length; row++){
+			System.out.print(rows.substring(row, row+1)+" ");
+			for(int col = 0; col < plots[row].length; col++){
+					System.out.print(".");
+			}
+			System.out.println(" " + rows.substring(row, row+1));
+		}
+		System.out.println(columns.substring(0, plots[0].length+2));
 	}
 
 }
