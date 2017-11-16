2166d5f91e983d7068d41512841d9f161b28fc35
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-16 11:47:21 -0500

    push

--- KPop/src/alexErikGame/ErikFrontEnd.java
+++ KPop/src/alexErikGame/ErikFrontEnd.java
@@ -8,6 +8,7 @@ public class ErikFrontEnd implements AlexSupport{
 	private int userShips;
 	private int compShips;
 	private String message;
+	private AlexErikFleet[][] ships;
 	
 	public static void main(String[] args) {
 		ErikFrontEnd game = new ErikFrontEnd();
@@ -16,26 +17,32 @@ public class ErikFrontEnd implements AlexSupport{
 	
 	public ErikFrontEnd() {
 			backend = new AlexBackEnd(this);
+			CaveExplorer.initScanner();
 			userShips = 3;
 			compShips = 3;
 			message = null;
+			ships = backend.getFleet();
 	}
 	
 	private void startGame() {
 		AlexErikFleet[][] ships = backend.getFleet();
 		AlexErikFleet p = null;
 		displayFleet(ships);
+		makeShips();
 	/*
 		while(userShips > 0 && compShips > 0) {
-			displayFleet(ships);
 			displayShipsSunk(p);
 			displayHints();
+			playersTurn();
+			userShips = getUserShips();
+			compShips = getCompShips();
+			computerTurn();
+			userShips = getUserShips();
+			compShips = getCompShips();
 			//tell user if ship is in NESW
 			System.out.println("Where do you want to shoot?");
 			int[] coords = backend.getCoordInput();
-			
-			userShips = getUserShips();
-			compShips = getCompShips();
+		
 			
 		//	p = ships[coords[0]][coords[1]];
 		}
@@ -44,6 +51,37 @@ public class ErikFrontEnd implements AlexSupport{
 	*/	
 	}
 	
+	private void makeShips() {
+		String input = CaveExplorer.in.nextLine();
+		selectPosition(input);
+		CaveExplorer.in.nextLine();
+		System.out.println("Hi");
+		if(input.length() == 3 && input.substring(0,1).length() < backend.getFleet().length && input.substring(2,3).length() < backend.getFleet().length) {
+			CaveExplorer.in.nextLine();
+			//place ship
+			if(!ships[Integer.parseInt(input.substring(0,1))][Integer.parseInt(input.substring(2,3))].containsShip()) {
+				System.out.println("Hello");
+			}
+		}
+		
+	}
+	
+
+	private void selectPosition(String input) {
+		boolean vertical = false;
+		boolean horizontal = false;
+		System.out.println("Where do you want to place your 3 platform ship?"+"Press 'h' for horizontal or 'p' for vertical)");
+		if(input.equals("v")) {
+			vertical = true;
+			horizontal = false;
+		}else if(input.equals("h")) {
+			horizontal = true;
+			vertical = false;
+		} else {
+			System.out.println("Please enter a valid input. 'h' or 'p'.");
+		}
+	}
+
 	private void displayFleet(AlexErikFleet[][] ships) {
 		String rows = "0123456789";
 		String columns = "   0 1 2 3 4 5 6 7 8 9";
--- KPop/src/caveExplorer/CaveExplorer.java
+++ KPop/src/caveExplorer/CaveExplorer.java
@@ -35,6 +35,10 @@ public class CaveExplorer {
 			currentRoom.interpretInput(input);
 		}
 	}
+	
+	public static void initScanner(){
+		in = new Scanner(System.in);
+	}
 
 	private static void npcActions() {
 		for(NPC n: npcs) {
