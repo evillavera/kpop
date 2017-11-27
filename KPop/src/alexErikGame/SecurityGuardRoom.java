eb78a7d326f3af518831af124ac11bfdaa832f13
erikj <erikj@10.0.1.16>
2017-11-26 22:20:43 -0500

    fsf

--- KPop/src/alexErikGame/SecurityGuardRoom.java
+++ KPop/src/alexErikGame/SecurityGuardRoom.java
@@ -1,7 +1,9 @@
 package alexErikGame;
 
 import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
 import caveExplorer.NPCRoom;
+import samJenny.Idol;
 
 public class SecurityGuardRoom extends NPCRoom {
 
@@ -39,4 +41,47 @@ public class SecurityGuardRoom extends NPCRoom {
 			}
 		}
 	}	
+	
+	/*
+	 * public class IdolRoom extends CaveRoom {
+
+	private Idol idol;
+
+	public IdolRoom(String description) {
+		super(description);
+		idol = null;
+	}
+	
+	public String getContents() {
+		return "I";
+	}
+	
+	public String validKeys() {
+		return "wdsae";
+	}
+	
+	public void printAllowedEntry() {
+		CaveExplorer.print("You can only input 'W', 'A', 'S', 'D' to move or 'E' to interact.");
+	}
+	
+	public void performAction(int direction) {
+		if(direction == 4) {
+			idol.interact();
+		}
+		else {
+			super.performAction(direction);
+		}
+	}
+	
+	public String getDescription() {
+		return super.getDescription() + "\n"+ idol.returnDescription();
+	}
+
+	public void enterPerson(Idol i) {
+		idol = i;
+	}
+}
+	 */
+	
+	
 }
